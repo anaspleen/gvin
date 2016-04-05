a83274e54505423cb7ac8f07e194ebe7d90f6104
anaspleenfromwork <thomas.caiati@gmail.com>
2016-04-05 09:13:45 +0200

    ajout testU

---
+++ src/test/java/ft/tic/gvin/Test.java
@@ -0,0 +1,39 @@
+//
+// Fichier :
+//    Test.java, v 1.0 4 avr. 2016 17:51:08 
+//
+// Projet : 
+//   LAQME
+//
+//   
+// Date :
+//  Créé le 4 avr. 2016 à 17:51:08
+//
+// Historique :
+//
+// Révision 1.0 4 avr. 2016 17:51:08 caiatit
+// Création.
+//
+//
+package ft.tic.gvin;
+
+
+/**
+ * 
+ * @author Bull
+ * $Id$
+ * 
+ */
+public class Test
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
