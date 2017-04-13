3484f19807c0b4d53ee3787ad30dd767f12c1b22
Thomas CAIATI <tcaiati@capsys.eu>
2017-04-13 17:41:12 +0200

    init du paho

---
+++ ambrosia-services/src/test/java/paho/TestPahoPublisher.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package paho;
+
+import java.io.IOException;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+/**
+ * http://www.jitendrazaa.com/blog/java/snmp/creating-snmp-agent-server-in-java-using-snmp4j/
+ * http://www.jitendrazaa.com/blog/java/snmp/create-snmp-client-in-java-using-snmp4j/
+ * 
+ * @author anaspleen
+ *
+ */
+public class TestPahoPublisher {
+
+	public static void main(String[] args) throws IOException {
+		String topic = "TCA/ex";
+		String content = "Message from MqttPublishSample by TCA";
+		int qos = 2;
+		// String broker = "tcp://iot.eclipse.org:1883";
+		String broker = "tcp://192.168.1.119:1883";
+		String clientId = "JavaSample";
+		MemoryPersistence persistence = new MemoryPersistence();
+
+		try {
+			MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+			MqttConnectOptions connOpts = new MqttConnectOptions();
+			connOpts.setCleanSession(true);
+			System.out.println("Connecting to broker: " + broker);
+			sampleClient.connect(connOpts);
+			System.out.println("Connected");
+			System.out.println("Publishing message: " + content);
+			MqttMessage message = new MqttMessage(content.getBytes());
+			message.setQos(qos);
+			sampleClient.publish(topic, message);
+			System.out.println("Message published");
+			sampleClient.disconnect();
+			System.out.println("Disconnected");
+			System.exit(0);
+		} catch (MqttException me) {
+			System.out.println("reason " + me.getReasonCode());
+			System.out.println("msg " + me.getMessage());
+			System.out.println("loc " + me.getLocalizedMessage());
+			System.out.println("cause " + me.getCause());
+			System.out.println("excep " + me);
+			me.printStackTrace();
+		}
+	}
+}
